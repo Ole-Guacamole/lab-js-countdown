7bbfc00021d3b87fb3f9d67abee3cb1922fef35f
Sven Ole Spindler <s.spindler@gmx.de>
2024-06-27 17:02:42 +0200

    lab solved

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const DURATION = 10; // 10 seconds
-let remainingTime = DURATION; // Countdown starting from 10
-let timer = null; // Variable to store the interval
+//const DURATION = 10; // 10 seconds
+//let remainingTime = DURATION; // Countdown starting from 10
+//let timer = null; // Variable to store the interval
 
 
 
@@ -8,7 +8,12 @@ let timer = null; // Variable to store the interval
 
 // Your code goes here ...
 
-
+const startButton = document.getElementById("start-btn");
+startButton.addEventListener("click", () => {
+  startCountdown();
+  startButton.disabled = true;
+  
+});
 
 
 // ITERATION 2: Start Countdown
@@ -17,6 +22,24 @@ function startCountdown() {
 
 
   // Your code goes here ...
+  
+  let i = 10;
+  const timeLeft = document.getElementById("time")
+  
+  const intervalId = setInterval(function () {
+    if (i >= 0) {
+      console.log(i);
+      timeLeft.textContent = i; // Update the display
+    } else {
+      showToast();
+      clearInterval(intervalId);
+    }
+   
+    i--;
+  }, 1000);
+
+
+
 }
 
 
@@ -27,8 +50,12 @@ function showToast(message) {
   console.log("showToast called!");
 
   // Your code goes here ...
-
-
+  const toast = document.getElementById("toast"); // get the toast Card Element
+  toast.classList.add("show");
+  setTimeout(() => {
+    toast.classList.remove("show");
+    startButton.disabled = false;
+  }, 3000);
 
 
   // BONUS: ITERATION 4: TOAST CLOSE BUTTON
